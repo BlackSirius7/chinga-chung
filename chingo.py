27c1abdfd29a09eebb7a3cf30108bfd2773fdbc0
BlackSirius7 <khoylunts1998@gmail.com>
2023-02-01 20:52:09 +0400

    hi

---
+++ chingo.py
@@ -0,0 +1,23 @@
+#import random
+
+# def chinga_chung():
+#     yntrutyun = ["qar", "mkrat", "tuxt"]
+#     comp_yntrutyun = random.choice(yntrutyun)
+#     mer_yntrutyun = input(" qar, mkrat , tuxt? ")
+    
+#     while mer_yntrutyun not in yntrutyun:
+#         print("sxal mutqagrum!!!!")
+#         mer_yntrutyun = input(" qar, mkrat , tuxt? ")
+    
+#     if comp_yntrutyun == mer_yntrutyun:
+#         print("nicha!")
+#     elif comp_yntrutyun == "qar" and mer_yntrutyun == "mkrat":
+#         print("Duq partveciq!")
+#     elif comp_yntrutyun == "mkrat" and mer_yntrutyun == "tuxt":
+#         print("Duq partveciq!")
+#     elif comp_yntrutyun == "tuxt" and mer_yntrutyun == "qar":
+#         print("Duq partveciq!")
+#     else:
+#         print("Du haxtecir!")
+
+# chinga_chung()
